9d61134c310813c53009bacc9a439ffabbfc4652
Emma Bent <emmabent@Emmas-MacBook-Pro.local>
2026-07-02 13:54:10 +0930

    Add Capacitor iOS setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.solla.app',
+  appName: 'Solla',
+  webDir: 'build'
+};
+
+export default config;
